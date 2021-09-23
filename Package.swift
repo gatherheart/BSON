5db6a407c0dfffb59dacd894bc0dc6b22e6fa872
Milkybean <gatherheart@naver.com>
2021-09-24 01:40:56 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,12 +10,10 @@ let package = Package(
             targets: ["BSON"])
         ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
         .target(
             name: "BSON",
-            dependencies: ["NIO"]
         ),
         .testTarget(
             name: "BSONTests",
